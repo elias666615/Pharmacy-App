fa1ed4b312f405f175ab00002835b3534baca33d
elias666615 <eliasbbb46@gmail.com>
2021-11-24 13:05:17 +0200

    added basic navigation and some components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './authentication/login/login.component';
+import { SalesComponent } from './seller/sales/sales.component';
+import { NavigationComponent } from './seller/navigation/navigation.component';
+import { ProfileComponent } from './seller/profile/profile.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { HomeComponent } from './seller/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'seller', component: NavigationComponent, 
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'profile', component: ProfileComponent },
+      { path: 'sales', component: SalesComponent },
+    ]},
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { SalesComponent } from './seller/sales/sales.component';
+import { HomeComponent } from './seller/home/home.component';
+import { NavigationComponent } from './seller/navigation/navigation.component';
+import { ProfileComponent } from './seller/profile/profile.component';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SalesComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    ProfileComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
