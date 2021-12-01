5c906acbf76d0ed2062c60ef4513b9aac12576ba
Nicolas Al Khoury <nicolas.n.khoury@gmail.com>
2021-12-01 16:00:10 +0200

    signup

--- frontend/src/app/authentication/signup-buyer/signup-buyer.component.ts
+++ frontend/src/app/authentication/signup-buyer/signup-buyer.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 
 @Component({
@@ -9,17 +10,52 @@ import { FormBuilder } from '@angular/forms';
 })
 export class SignupBuyerComponent implements OnInit {
   signupForm = this.fb.group({
-
-    firstname: [''],
-    lastname: [''],
-    phone: [''],
-    email: [''],
-    password: [''],
-    confirmPassword: [''],
+    pharmacyname: ['', [Validators.required]],
+    phone: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+    confirmPassword: ['', [Validators.required]],
+    location: ['', [Validators.required]],
   });
+
+  signupDisabled: boolean = true;
+  @ViewChild('termCheck') TermCheck: any;
+  @ViewChild('fileBrowseRef') fileBrowseRef: any;
+  imageFileName: string = '';
+
+  checkIfDisabled() {
+    console.log('test');
+    this.signupDisabled = !this.signupForm.valid || !this.TermCheck.nativeElement.checked;
+  }
+
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
+  imageUrl!: any;
 
+  fileBrowserHandler(event: any) {
+    if (!event.target.files[0] || event.target.files[0].length == 0) {
+      return;
+    }
+
+    var mimeType = event.target.files[0].type;
+
+    if (mimeType.match(/image\/*/) == null) {
+      return;
+    }
+
+    this.imageUrl = event.target.files[0];
+    this.imageFileName = event.target.files[0].name;
+    console.log(event.target.files[0])
+    var reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+
+    reader.onload = (_event) => {
+      //this.imageUrl = reader.result;
+    };
+  }
+  browseImage() {
+    this.fileBrowseRef.nativeElement.click();
+  }
 }
